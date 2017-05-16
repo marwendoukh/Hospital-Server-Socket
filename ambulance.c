2f88b37fef68afd1450cdf31911aedffb77ff166
Marwen Doukh <marwen.doukh@gmail.com>
2017-05-16 12:59:52 +0100

    send multiple msg to server

--- ambulance.c
+++ ambulance.c
@@ -5,13 +5,13 @@
 #include<string.h>    //strlen
 #include<sys/socket.h>    //socket
 #include<arpa/inet.h> //inet_addr
- 
+
 int main(int argc , char *argv[])
 {
     int sock;
     struct sockaddr_in server;
     char message[1000] , server_reply[2000];
-     
+
     //Create socket (TCP socket)
     sock = socket(AF_INET , SOCK_STREAM , 0);
     if (sock == -1)
@@ -19,51 +19,47 @@ int main(int argc , char *argv[])
         printf("Could not create socket");
     }
     puts("Socket created");
-    // address of the server 
+    // address of the server
     server.sin_addr.s_addr = inet_addr("192.168.43.191");
     server.sin_family = AF_INET;
     server.sin_port = htons( 8888 );
- 
+
     //Connect to remote server
     if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
     {
         perror("connect failed. Error");
         return 1;
     }
-     
+
     puts("Connected\n");
-     
+
     //keep communicating with server
     while(1)
     {
-       // printf("Enter message : ");
-        //scanf("%s" , message);
-         
+        printf("Enter message : ");
+        scanf("%s" , message);
+
         //Send request to server
-        if( send(sock , "could I come ?" , strlen(message) , 0) < 0)
+        if( send(sock , message , strlen(message) , 0) < 0)
         {
             puts("Send failed");
             return 1;
         }
-         
+
         //Receive a reply from the server
         if( recv(sock , server_reply , 2000 , 0) < 0)
         {
             puts("recv failed");
             break;
         }
-         
-        
-	if (server_reply == "ok")
-	{
-	    puts("Hospital Accepted our request");
-	}
-	else if (server_reply == "no")
-	{
-	    puts("Hospital Rejected our request");
-	}
+
+
+
+	    puts(server_reply);
+
+
     }
-     
+
     close(sock);
     return 0;
 }
